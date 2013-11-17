62ab994bba5d664977c4e7de7ff3022506455bb6
unknown <Michel>
2013-11-17 18:55:53 +1100

    fixes

--- bundle.php
+++ bundle.php
@@ -2,15 +2,11 @@
 namespace App\Newsletter;
 
 class Bundle extends \Coxis\Core\BundleLoader {
-	public function load($queue) {
-		parent::load();
-	}
-
 	public function run() {
-		AdminMenu::$menu[0]['childs'][] = array('label' => 'Newsletter', 'link' => 'newsletter');
-		\Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('newsletter/icon.svg'), 'link'=>'newsletter', 'title' => __('Newsletter'), 'description' => __('Mange your newsletters.'));
-		AdminMenu::$menu[0]['childs'][] = array('label' => __('Subscribers'), 'link' => 'subscribers');
-		\Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('newsletter/subscribers-icon.svg'), 'link'=>'subscribers', 'title' => __('Subscribers'), 'description' => __('Subscribers of your newsletter.'));
+		AdminMenu::instance()->menu[0]['childs'][] = array('label' => 'Newsletter', 'link' => 'newsletter');
+		\Coxis\Admin\Libs\AdminMenu::instance()->home[] = array('img'=>\URL::to('newsletter/icon.svg'), 'link'=>'newsletter', 'title' => __('Newsletter'), 'description' => __('Mange your newsletters.'));
+		AdminMenu::instance()->menu[0]['childs'][] = array('label' => __('Subscribers'), 'link' => 'subscribers');
+		\Coxis\Admin\Libs\AdminMenu::instance()->home[] = array('img'=>\URL::to('newsletter/subscribers-icon.svg'), 'link'=>'subscribers', 'title' => __('Subscribers'), 'description' => __('Subscribers of your newsletter.'));
 		parent::run();
 	}
 }
